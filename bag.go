6e27ba27559f75f8417f8c817ab1b28c1e579fe0
Streamweaver <Streamweaver@gmail.com>
2013-05-27 23:56:15 -0400

    Added basic lisence information.

--- bag.go
+++ bag.go
@@ -18,6 +18,6 @@ func NewBag(location string, name string) *Bag {
 	// TODO Check that location exists.
 	// TODO Make a directory with name in location.
 	// TODO Make a data diretory in the named bag.
-	// 
+	// TODO Initialize a bag-info.txt file.
 	return new(Bag)
 }
