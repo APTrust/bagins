c9eabec80253f2873aa4acb80f94f5e10f6e2025
Streamweaver <Streamweaver@gmail.com>
2013-05-30 17:32:48 -0400

    Updated manifests.

--- manifest.go
+++ manifest.go
@@ -1,10 +1,3 @@
-/* Manages manifest format files in Bags.  As of BagIt spec 0.97 this means
-   only manifest-<algo>.txt and tagmanifest-<algo>.txt files.
-
-   For more information see:
-	 manifest: http://tools.ietf.org/html/draft-kunze-bagit-09#section-2.1.3
-	 tagmanifest: http://tools.ietf.org/html/draft-kunze-bagit-09#section-2.2.1
-*/
 package bagins
 
 import (
@@ -16,6 +9,12 @@ import (
 	"strings"
 )
 
+// Manifest represents information about a BagIt manifest file.  As of BagIt spec
+// 0.97 this means only manifest-<algo>.txt and tagmanifest-<algo>.txt files.
+//
+// For more information see:
+//   manifest: http://tools.ietf.org/html/draft-kunze-bagit-09#section-2.1.3
+//   tagmanifest: http://tools.ietf.org/html/draft-kunze-bagit-09#section-2.2.1
 type Manifest struct {
 	Data     map[string]string // Map of filepath key and checksum value for that file
 	Path     string            // Path to the Manifest File
