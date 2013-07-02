4e0981064f4ec679cefe82511c7f31b0ebdc5930
Streamweaver <Streamweaver@gmail.com>
2013-07-01 23:13:27 -0400

    Enabled basic commandline arguments.

--- main/bagins.go
+++ main/bagins.go
@@ -4,76 +4,71 @@ package main
 import (
 	"flag"
 	"fmt"
-	// "github.com/APTrust/bagins"
-	// "github.com/APTrust/bagins/bagutil"
+	"github.com/APTrust/bagins"
+	"github.com/APTrust/bagins/bagutil"
 )
 
-var algo string
-var baseDir string
-var bagName string
-var srcDir string
-var help bool
-var opts map[string]*option
-
-type option struct {
-	full string
-	df   string
-	help string
-}
-
-func newOption(full string, df string, help string) *option {
-	opt := new(option)
-	opt.full = full
-	opt.df = df
-	opt.help = help
-	return opt
-}
+var (
+	dir     string
+	name    string
+	payload string
+	algo    string
+)
 
 func init() {
-	opts["algo"] = newOption("algo", "md5", "Hash type to use for checksums.")
-	opts["dir"] = newOption("dir", "", "Destination directory for the bag.")
-	opts["name"] = newOption("name", "", "Name of the bag root dirctory.")
-	opts["src"] = newOption("src", "", "Directory containing the files to bag.")
+	flag.StringVar(&dir, "dir", "", "Directory to create the bag.")
+	flag.StringVar(&name, "name", "", "Name for the bag root directory.")
+	flag.StringVar(&payload, "payload", "", "Directory of files to parse into the bag")
+	flag.StringVar(&algo, "algo", "md5", "Checksum algorithm to use.  md5, sha1, sha224, sha256, sha512, sha384")
 
-	flag.StringVar(&algo, "algo", opt["algo"]["default"], opt["algo"]["help"])
-	flag.StringVar(&baseDir, "dir", opt["dir"]["default"], opt["dir"]["help"])
-	flag.StringVar(&bagName, "name", "", "Name of the bag root directory.")
-	flag.BoolVar(&help, "h", false, "Show help for this tool.")
+	flag.Parse()
 }
 
 func usage() {
-	usg := `Usage: 
-				go run bagins.go -dir dirctory -name name -filedir directory [-algo algo]
+	fmt.Println(`
+Usage:
+	go run baging.go -dir <value> -name <value> -payload <value> [-algo <value>]
 
-			Flags:
-				`
-	fmt.Println(usg)
-	for _, opt := range opts {
-		fmt.Println("	", "-"+opt["name"], opt["help"])
+Flags:
+		`)
+	printFlags := func(a *flag.Flag) {
+		fmt.Println("	-"+a.Name+" <value>", a.Usage)
 	}
+
+	flag.VisitAll(printFlags)
 }
 
 func main() {
-	flag.Parse()
-	usage()
 
-	// BASIC CODE BELOW.
-	// cs, err := bagutil.NewCheckByName(algo)
-	// if err != nil {
-	// 	fmt.Println("Unable to find checksum", algo)
-	// 	return
-	// }
-	// bag, err := bagins.NewBag(`C:\tmp`, "bag-all-pictures", cs)
-	// if err != nil {
-	// 	fmt.Println("Bag Error:", err)
-	// 	return
-	// }
-	// errs := bag.AddDir(`E:\Pictures`)
-	// for err := range errs {
-	// 	fmt.Println("AddDir Error:", err)
-	// 	return
-	// }
-	// bag.Close()
-	// fmt.Println("Done!")
-	// return
+	if dir == "" {
+		usage()
+		return
+	}
+	if name == "" {
+		usage()
+		return
+	}
+	if payload == "" {
+		usage()
+		return
+	}
+
+	cs, err := bagutil.NewCheckByName(algo)
+	if err != nil {
+		fmt.Println("Unable to find checksum", algo)
+		return
+	}
+	bag, err := bagins.NewBag(dir, name, cs)
+	if err != nil {
+		fmt.Println("Bag Error:", err)
+		return
+	}
+	errs := bag.AddDir(payload)
+	for err := range errs {
+		fmt.Println("AddDir Error:", err)
+		return
+	}
+	bag.Close()
+	fmt.Println("Done!")
+	return
 }
