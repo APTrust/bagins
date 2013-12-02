b63453ad9bde1aa793d0c91e55a0d2af3a05c5be
Streamweaver <Streamweaver@gmail.com>
2013-12-02 17:01:44 -0500

    Finished off TagField and TagFieldList tests.

--- tagfile.go
+++ tagfile.go
@@ -18,20 +18,99 @@ import (
 	"strings"
 )
 
+// TAG FIELD
+
+// Represents a tag field as referenced in the standard BagIt tag file and used
+// in bag-info.txt.  It represents a standard key value pair with the label with corresponding
+// value.  For more information see
+// http://tools.ietf.org/html/draft-kunze-bagit-09#section-2.2.2
+type TagField struct {
+	label string // Name of the tag field
+	value string // Value of the tag field
+}
+
+func NewTagField(label string, value string) *TagField {
+	return &TagField{label, value}
+}
+
+func (f *TagField) Label() string {
+	return f.label
+}
+
+func (f *TagField) SetLabel(l string) {
+	f.label = l
+}
+
+func (f *TagField) Value() string {
+	return f.value
+}
+
+func (f *TagField) SetValue(v string) {
+	f.value = v
+}
+
+// TAG FIELD LIST
+
+// Ordered list of fields used in a TagFile.
+type TagFieldList struct {
+	fields []TagField // Some useful manipulations in https://code.google.com/p/go-wiki/wiki/SliceTricks
+}
+
+func NewTagFieldList() *TagFieldList {
+	return new(TagFieldList)
+}
+
+func (fl *TagFieldList) Fields() []TagField {
+	return fl.fields
+}
+
+func (fl *TagFieldList) SetFields(fields []TagField) {
+	fl.fields = fields
+}
+
+func (fl *TagFieldList) AddField(field TagField) {
+	fl.fields = append(fl.Fields(), field)
+}
+
+func (fl *TagFieldList) RemoveField(i int) error {
+	if i+1 > len(fl.Fields()) || i < 0 {
+		return errors.New("Invalid index for TagField")
+	}
+	if len(fl.fields) == i {
+		fl.fields = fl.Fields()[:i]
+		return nil
+	}
+	fl.fields = append(fl.Fields()[:i], fl.Fields()[i+1:]...)
+	return nil
+}
+
+// TAG FILES
+
 type TagFile struct {
 	name string            // Filepath for tag file.
 	Data map[string]string // key value pairs of data for the tagfile.
 }
 
+// Creates a new tagfile object and returns it or returns an error if improperly formatted.
+// The name argument represents the filepath of the tagfile, which must end in txt
 func NewTagFile(name string) (tf *TagFile, err error) {
-	_, err = os.Stat(filepath.Dir(name))
-	re, _ := regexp.Compile(`.*\.txt`)
-	if !re.MatchString(filepath.Base(name)) {
-		err = errors.New(fmt.Sprint("Tagfiles must end in .txt and contain at least 1 letter.  Provided: ", filepath.Base(name)))
-	}
+	err = validateTagFileName(name)
 	tf = new(TagFile)
 	tf.name = filepath.Clean(name)
 	tf.Data = make(map[string]string)
+	return tf, err
+}
+
+// Opens a tagfile, parsing the contents as tagfile field data and returning the TagFile object.
+// name is the filepath to the tag file.  It throws an error if contents cannot be properly parsed.
+func OpenTagFile(name string) (*TagFile, error) {
+	file, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	tf, err := NewTagFile(name)
 	return tf, err
 }
 
@@ -102,3 +181,20 @@ func FormatField(key string, data string) (string, error) {
 	}
 	return buff.String(), nil
 }
+
+// Some private convenence methods for manipulating tag files.
+func validateTagFileName(name string) (err error) {
+	_, err = os.Stat(filepath.Dir(name))
+	re, _ := regexp.Compile(`.*\.txt`)
+	if !re.MatchString(filepath.Base(name)) {
+		err = errors.New(fmt.Sprint("Tagfiles must end in .txt and contain at least 1 letter.  Provided: ", filepath.Base(name)))
+	}
+	return err
+}
+
+// Reads the contents of file and parses tagfile fields from the contents or returns an error if
+// it contains unparsable data.
+// func parseFields(data map[string]string, file *os.File) (err error) {
+// 	re, err := regexp.Compile(`^\S*\:`)
+// 	return err
+// }
--- tagfile_test.go
+++ tagfile_test.go
@@ -2,12 +2,158 @@
 package bagins_test
 
 import (
+	"fmt"
 	"github.com/APTrust/bagins"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"testing"
 )
+
+// Field TESTS
+
+func TestNewField(t *testing.T) {
+	exp_label := "test-field"
+	exp_value := "this is my test"
+	f := bagins.NewTagField(exp_label, exp_value)
+
+	if f == nil { // This should test type but for the life of me I can't figure out how.
+		t.Error("Tag Field object not returned")
+	}
+	if f.Label() != exp_label {
+		t.Error("Tag Field label not created properly!")
+	}
+	if f.Value() != exp_value {
+		t.Error("Tag Field value not created properly!")
+	}
+}
+
+// Using a single test for set and get labels since they rely on one another.
+func TestLabel(t *testing.T) {
+	f := bagins.NewTagField("label", "value")
+	if f.Label() != "label" {
+		t.Error("Tag Field label not created properly!")
+	}
+
+	f.SetLabel("new-label")
+	if f.Label() != "new-label" {
+		t.Error("Tag Field label not reset properly!")
+	}
+
+	if f.Value() != "value" {
+		t.Error("Tag Field value not set or retained properly when label changed!")
+	}
+}
+
+// Using single test for set and get values since they rely on one another.
+func TestValue(t *testing.T) {
+	f := bagins.NewTagField("label", "value")
+	if f.Value() != "value" {
+		t.Error("Tag Field value not created properly!")
+	}
+
+	f.SetValue("new value")
+	if f.Value() != "new value" {
+		t.Error("Tag Field value not set or read properly!")
+	}
+
+	if f.Label() != "label" {
+		t.Error("Tag Field label value not retained when value set!")
+	}
+}
+
+// FieldList TESTS
+
+func TestNewTagFieldList(t *testing.T) {
+	var tfl interface{} = bagins.NewTagFieldList()
+	if _, ok := tfl.(*bagins.TagFieldList); !ok {
+		t.Error("TagFieldList not returned!")
+	}
+}
+
+// Doing a unified test for Fields and SetFields
+func TestFields(t *testing.T) {
+
+	fl := bagins.NewTagFieldList()
+	test_len := func(l int) { // DRY!
+		if len(fl.Fields()) != l {
+			t.Error("Expected TagField length of", l, "but", len(fl.Fields()), "was returned!")
+		}
+	}
+
+	test_len(0)
+
+	newFields := []bagins.TagField{
+		*bagins.NewTagField("label1", "value1"),
+		*bagins.NewTagField("label2", "value2"),
+		*bagins.NewTagField("label3", "value3"),
+	}
+	fl.SetFields(newFields)
+	test_len(3)
+
+	for i := 0; i < 3; i++ {
+		exp := fmt.Sprintf("label%d", i+1)
+		act := fl.Fields()[i].Label()
+		if exp != act {
+			t.Error("Expected", exp, "but returned", act)
+		}
+	}
+}
+
+func TestAddField(t *testing.T) {
+	fl := bagins.NewTagFieldList()
+	exp_len := 100
+	for i := 0; i < exp_len; i++ {
+		tmp := strconv.Itoa(i)
+		fl.AddField(*bagins.NewTagField(tmp, tmp))
+	}
+
+	if len(fl.Fields()) != exp_len {
+		t.Error("Expected", exp_len, "fields but returned", len(fl.Fields()), "!")
+	}
+
+	for i, f := range fl.Fields() {
+		if f.Value() != strconv.Itoa(i) {
+			t.Error("Expected field value of", strconv.Itoa(i), "but returned", f.Value(), "!")
+		}
+	}
+}
+
+func TestRemoveField(t *testing.T) {
+	fl := bagins.NewTagFieldList()
+	test_len := func(l int) { // DRY again!
+		if len(fl.Fields()) != l {
+			t.Error("Expected TagField length of", l, "but", len(fl.Fields()), "was returned!")
+		}
+	}
+
+	for i := 0; i < 100; i++ {
+		tmp := strconv.Itoa(i)
+		fl.AddField(*bagins.NewTagField(tmp, tmp))
+	}
+	test_len(100)
+
+	// Should error if removing out of range.
+	if err := fl.RemoveField(-6); err == nil {
+		t.Error("Trying to remove negative index does not produce expected error!")
+	}
+	if err := fl.RemoveField(100); err == nil {
+		t.Error("Trying to remove out of bound index does not produce expected error!")
+	}
+	test_len(100)
+
+	// Remove every other one of the first 25 and test
+	for i := 0; i < 50; i++ {
+		if i%2 == 0 {
+			fl.RemoveField(i)
+		}
+	}
+	test_len(75)
+
+}
+
+// TagFile TESTS
 
 func TestNewTagFile(t *testing.T) {
 	_, err := bagins.NewTagFile("tagfile.txt")
